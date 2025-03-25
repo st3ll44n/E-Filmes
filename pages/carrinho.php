b78102650081511226c255eb64384fc029d8fcc5
st3ll44n <stellanaziozeno@gmail.com>
2025-03-25 16:16:56 -0300

    Uptades para aprimoração

--- pages/carrinho.php
+++ pages/carrinho.php
@@ -19,13 +19,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active text-light" aria-current="page" href="../index.html">Home</a>
+                    <a class="nav-link active text-light" aria-current="page" href="../index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-light" href="catalogo.html">Catalogo</a>
+                    <a class="nav-link text-light" href="catalogo.php">Catalogo</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-light" href="carrinho.html">Carrinho</a>
+                    <a class="nav-link text-light" href="carrinho.php">Carrinho</a>
                 </li>
                 </ul>
                 <form class="d-flex" role="search">
